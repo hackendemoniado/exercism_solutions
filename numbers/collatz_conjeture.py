24c917c9e14d872c16c0d295bfaa378243cf4f7d
hackendemoniado <12801816+hackendemoniado@users.noreply.github.com>
2025-09-11 10:25:54 -0300

    Refactor: optimize collatz_conjeture.py

--- numbers/collatz_conjeture.py
+++ numbers/collatz_conjeture.py
@@ -1,26 +1,11 @@
-def is_even(number):
-    return number % 2 == 0
+def steps(number: int) -> int:
+    if number <= 0:
+        raise ValueError("Only positive integers are allowed")
 
-def is_odd(number):
-    return number % 2 == 1
+    steps = 0
+    while number != 1:
+        number = number // 2 if number %2 == 0 else number * 3 + 1
+        steps += 1
+    return steps
 
-def steps(number: int) -> bool:
-    try:
-        if number <= 0:
-            raise ValueError("Only positive integers are allowed")
-        else:
-            steps = 0
-            while number != 1:
-                if is_even(number):
-                    number /= 2
-                    steps += 1
-                elif is_odd(number):
-                    number *= 3
-                    number += 1
-                    steps += 1
-            return steps
-
-    except ValueError as e:
-        print(f"Error: {e}")
-        raise
 print(steps(12))
